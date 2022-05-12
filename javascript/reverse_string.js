fb6ccec4ebc8bb621671f102ecc85747364ec3ae
Matt Quinn <mjq0533@gmail.com>
2022-05-12 16:17:10 -0500

    complete

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,11 +1,19 @@
 function reverseString(str) {
   // type your code here
+  let reversed = "";
+
+  for (let i = str.length - 1; i > -1; --i ) {
+    reversed = reversed + str[i];
+  }
+  
+  return reversed
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
+
 
   console.log("");
 
